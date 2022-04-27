3edfd881f9d1413c9c10f02d2197e8ef4f0ca016
Your Name <you@example.com>
2022-04-27 14:17:16 -0500

    first commit

--- main.py
+++ main.py
@@ -60,5 +60,5 @@ def deleteProduct(product_name):
             'products': products
         })
 
-if __name__ == '__main__':
-    app.run(debug=True, port=4000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
